9d190894ea84732ba95773fe332586557289e044
Alex Whyatt <59887470+CptBarnacles@users.noreply.github.com>
2020-03-24 15:17:13 +0000

    rakes completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,34 +17,3 @@ class ApplicationController < ActionController::Base
 		end
 	end
 end
-
-
-
-module SessionsHelper
-  
- def log_in(user)
-    session[:user_id] = user.id
-  end
-
- def current_user
-   @current_user ||= User.find_by(id:session[:user_id])
- end
-
- def logged_in?
-   #!current_user.nil?
-    unless User.find_by(id: session[:user_id])
-     redirect_to root_url, info:"Please log in"
-   end
- end
-
- def admin?
- 	unless @current_user && @current_user.admin
- 		redirect_to root_url, warning: "Unauthorized access"
- 	end
- end
-
- def log_out
-   session.delete(:user_id)
-   @current_user = nil
- end
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-	has_many :sponsors
+	has_many :sponsors, dependent: :destroy
 	has_many :orders
+	paginates_per 10
 	validates :name,  presence: true, length:{maximum: 50 }
   VALID_EMAIL_REGEX =/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 75 },
