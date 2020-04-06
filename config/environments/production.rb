301dc53cb2e8e01afab5e98479f2cc0a2a3f24d9
Alex Whyatt <59887470+CptBarnacles@users.noreply.github.com>
2020-04-06 13:50:15 +0100

    removed session key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,10 +61,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "planets_#{Rails.env}"
-  config.action_controller.session = { 
-      :session_key => "_user_session",  
-      :secret      => "6h54gjh254g747171g7l4h1g74l4g1" 
-  }
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
